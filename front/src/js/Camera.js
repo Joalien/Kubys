a422572ee2fcfc822f7d176faa6706ee894e9fce
CORNEC <josquin.cornec@insa-lyon.fr>
2019-05-01 01:57:20 +0200

    1.4: new tree generator

--- front/src/js/Camera.js
+++ front/src/js/Camera.js
@@ -3,12 +3,14 @@ export default class Camera {
     constructor(scene, canvas) {
         this.canvas = canvas;
         this.scene = scene;
-        this.universalCamera = new BABYLON.UniversalCamera("UniversalCamera", new BABYLON.Vector3(0, 20, -50), scene);
-        this.universalCamera.setTarget(new BABYLON.Vector3.Zero());
-        this.setUniversalCamera();
-
         this.arcRotateCamera = new BABYLON.ArcRotateCamera("ArcRotateCamera", 10, 0.8, 10, new BABYLON.Vector3(0, 0, 0), scene);
         this.arcRotateCamera.setPosition(new BABYLON.Vector3(0, 30, -30));
+        this.setArcRotateCamera();
+
+
+
+        this.universalCamera = new BABYLON.UniversalCamera("UniversalCamera", new BABYLON.Vector3(0, 20, -50), scene);
+        this.universalCamera.setTarget(new BABYLON.Vector3.Zero());
     }
 
 
--- front/src/js/Map.js
+++ front/src/js/Map.js
@@ -1,14 +1,15 @@
 import * as Materials from 'babylonjs-materials';
 import "babylonjs-loaders";
 import Grass from "../../textures/grass.jpg"
+import Tree from "../../textures/tree.jpg"
+import Leaf from "../../textures/leaf.jpg"
 
 export default class Map {
-
+    
     constructor(game) {
         // Appel des variables nécéssaires
         this.game = game;
         let scene = game.scene;
-        const cubeSize = 1;
 
         // Création de notre lumière principale
         let light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
@@ -44,8 +45,16 @@ export default class Map {
         // textureAssetTask.onError = function (){
         //     console.log("onError");
         // };
+                
+        this.createFloor(scene);
 
+        this.createTree(scene, -15, 5);
+        this.createTree(scene, 2, -10);
 
+    }
+
+    createFloor(scene){
+        const cubeSize = 1;
         let tex = new BABYLON.Texture(Grass, scene);
         //Creation of a material
         const grassMaterial = new BABYLON.StandardMaterial("groundTexture", scene);
@@ -63,6 +72,41 @@ export default class Map {
             for (let j = 0; j < sizeOfMap; j++) {
                 let secondaryBox = mainBox.clone();
                 secondaryBox.position = new BABYLON.Vector3(-sizeOfMap / 2 * cubeSize + cubeSize * i, cubeSize / 2, -sizeOfMap / 2 * cubeSize + cubeSize * j);
+            }
+        }
+    }
+
+    createTree(scene, x, z){
+        const cubeSize = 1;
+        let treeTexture = new BABYLON.Texture(Tree, scene);
+        let leafTexture = new BABYLON.Texture(Leaf, scene);
+        //Creation of a material
+        const treeMaterial = new BABYLON.StandardMaterial("treeTexture", scene);
+        treeMaterial.diffuseTexture = treeTexture;
+        const leafMaterial = new BABYLON.StandardMaterial("leafTexture", scene);
+        leafMaterial.diffuseTexture = leafTexture;
+
+        // SUR TOUS LES AXES Y -> On monte les meshes de la moitié de la hauteur du mesh en question.
+        let tree = BABYLON.Mesh.CreateBox("tree", cubeSize, scene);
+        tree.position = new BABYLON.Vector3(x, cubeSize / 2 + cubeSize, z);
+        tree.scaling = new BABYLON.Vector3(0.95, 0.95, 0.95);
+        tree.material = treeMaterial;
+        for (let i = 1; i < 4; i++) {
+            let secondaryBox = tree.clone();
+            secondaryBox.position.y = tree.position.y + cubeSize * i;
+        }
+
+        let leaf = BABYLON.Mesh.CreateBox("leaf", cubeSize, scene);
+        leaf.position = new BABYLON.Vector3(x, cubeSize / 2 + cubeSize + cubeSize * 4, z);
+        leaf.scaling = new BABYLON.Vector3(0.95, 0.95, 0.95);
+        leaf.material = leafMaterial;
+        for (let i = -1; i <= 1; i++) {
+            for (let j = -1; j <= 1; j++) {
+                if(i===0 && j===0) continue;
+                let secondaryBox = leaf.clone();
+                secondaryBox.position.x = x + i;
+                secondaryBox.position.z = z + j;
+                secondaryBox.position.y = leaf.position.y - cubeSize / 2;
             }
         }
     }
