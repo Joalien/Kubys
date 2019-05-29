f59de4ca5921bbf907b70129423fa50293f5ac87
Josquin <jcornec@takima.fr>
2019-05-29 14:26:25 +0200

    Remove local ip from deploy sripts

--- front/deploy.sh
+++ front/deploy.sh
@@ -3,10 +3,10 @@
 npm run build 
 cp -R textures/ dist/
 
-scp -r Dockerfile dist/ josquin@192.168.1.2:~/front
+scp -r Dockerfile dist/ josquin@kubys.fr:~/front
 
 #Maybe think about destroy all running containers
-ssh josquin@192.168.1.2 -t "cd ~/front && 
+	ssh josquin@kubys.fr -t "cd ~/front && 
 	docker build -t kubys_frontend . && 
 	docker stop $(docker ps | grep kubys_frontend | cut -d " " -f1) ;
 	docker run -d -p 80:80 -p 443:443 -v /home/josquin/front/certs/:/etc/ssl  kubys_frontend"
